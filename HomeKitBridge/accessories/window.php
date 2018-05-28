b6ce5a36df5397a9556498bb04c00fde2e9272e3
mvs90 <39708178+mvs90@users.noreply.github.com>
2018-05-28 22:12:04 +0200

    Update window.php

--- HomeKitBridge/accessories/window.php
+++ HomeKitBridge/accessories/window.php
@@ -1,11 +1,7 @@
 <?php
-
 declare(strict_types=1);
-
 class HAPAccessoryWindow extends HAPAccessoryBase
 {
-    use HelperSetDevice;
-
     public function __construct($data)
     {
         parent::__construct(
@@ -16,156 +12,134 @@ class HAPAccessoryWindow extends HAPAccessoryBase
             ]
         );
     }
-	
-	public function notifyCharacteristicTargetPosition()
-    {
-        return [$this->data['TargetPosition']];
-    }
-	
-	public function readCharacteristicTargetPosition()
+    public function readCharacteristicTargetPosition()
     {
         return GetValue($this->data['TargetPosition']);
     }
-	
-	public function notifyCharacteristicCurrentPosition()
-    {
-        return [$this->data['CurrentPosition']];
-    }
-
     public function readCharacteristicCurrentPosition()
     {
         return GetValue($this->data['CurrentPosition']);
     }
-
-	public function notifyCharacteristicPositionState()
+    public function readCharacteristicPositionState()
     {
-        return [$this->data['PositionState']];
+        return GetValue($this->data['PositionState']);
     }
-	
-	public function readCharacteristicPositionState()
-    {
-       return GetValue($this->data['PositionState']);
-    }
-	
-	public function writeCharacteristicTargetPosition($value)
+    public function writeCharacteristicTargetPosition($value)
     {
         $this->switchDevice($this->data['TargetPosition'], $value);
     }
-	
-	public function writeCharacteristicCurrentPosition($value)
+    public function writeCharacteristicCurrentPosition($value)
     {
         $this->switchDevice($this->data['CurrentPosition'], $value);
     }
-
     public function writeCharacteristicPositionState($value)
     {
-	 $this->switchDevice($this->data['PositionState'], $value);
+        $this->switchDevice($this->data['PositionState'], $value);
     }
-    
     protected function switchDevice($variableID, $value)
     {
-	 $targetVariable = IPS_GetVariable($variableID);
- 
-	 if ($targetVariable['VariableCustomAction'] != '') {
-             $profileAction = $targetVariable['VariableCustomAction'];
-         } else {
-             $profileAction = $targetVariable['VariableAction'];
-         }
-	    
-         if ($profileAction < 10000) {
-             echo 'No action was defined!';
- 
-             return;
-         }
- 
-         if (IPS_InstanceExists($profileAction)) {
-             IPS_RunScriptText('IPS_RequestAction(' . var_export($profileAction, true) . ', ' . var_export(IPS_GetObject($variableID)['ObjectIdent'], true) . ', ' . var_export($value, true) . ');');
-         } elseif (IPS_ScriptExists($profileAction)) {
-             IPS_RunScriptEx($profileAction, ['VARIABLE' => $variableID, 'VALUE' => $value, 'SENDER' => 'VoiceControl']);
-         }
-     }
+        $targetVariable = IPS_GetVariable($variableID);
+        if ($targetVariable['VariableCustomAction'] != '') {
+            $profileAction = $targetVariable['VariableCustomAction'];
+        } else {
+            $profileAction = $targetVariable['VariableAction'];
+        }
+        if ($profileAction < 10000) {
+            echo 'No action was defined!';
+            return;
+        }
+        if ($targetVariable['VariableType'] == 0 /* Boolean */) {
+            $value = boolval($value);
+        } elseif ($targetVariable['VariableType'] == 1 /* Integer */) {
+            $value = intval($value);
+        } elseif ($targetVariable['VariableType'] == 2 /* Float */) {
+            $value = floatval($value);
+        } else {
+            echo 'Strings are not supported';
+            return;
+        }
+        if (IPS_InstanceExists($profileAction)) {
+            IPS_RunScriptText('IPS_RequestAction(' . var_export($profileAction, true) . ', ' . var_export(IPS_GetObject($variableID)['ObjectIdent'], true) . ', ' . var_export($value, true) . ');');
+        } elseif (IPS_ScriptExists($profileAction)) {
+            IPS_RunScriptEx($profileAction, ['VARIABLE' => $variableID, 'VALUE' => $value, 'SENDER' => 'WebFront']);
+        }
+    }
 }
-
 class HAPAccessoryConfigurationWindow
 {
-	use HelperSetDevice;
-	
-	public static function getPosition()
-	{
-		return 90;
-	}
-	
-	public static function getCaption()
-	{
-       return 'Window';
-	}
-		
-	public static function getColumns()
-	{
-		return [
-			[
-				'label' => 'TargetPosition',
-				'name'  => 'TargetPosition',
-				'width' => '150px',
-				'add'   => 0,
-				'edit'  => [
-					'type' => 'SelectVariable'
-				]
-			],
-			[
-				'label' => 'CurrentPosition',
-				'name'  => 'CurrentPosition',
-				'width' => '150px',
-				'add'   => 0,
-				'edit'  => [
-					'type' => 'SelectVariable'
-				]
-			],
-			[
-				'label' => 'PositionState',
-				'name'  => 'PositionState',
-				'width' => '150px',
-				'add'   => 0,
-				'edit'  => [
-					'type' => 'SelectVariable'
-				]
-			]
-		];
-	}
-		
-	public static function getStatus($data)
-	{
-		if (!IPS_VariableExists($data['TargetPosition'])) {
-			return 'TargetPosition missing';
-		}
-		if (!IPS_VariableExists($data['CurrentPosition'])) {
-			return 'CurrentPosition missing';
-		}
-		if (!IPS_VariableExists($data['PositionState'])) {
-			return 'PositionState missing';
-		}
-		$variableTargetPosition = IPS_GetVariable($data['TargetPosition']);
-		$variableCurrentPosition = IPS_GetVariable($data['CurrentPosition']);
-		$variablePositionState = IPS_GetVariable($data['PositionState']);
-		if ($variableTargetPosition['VariableType'] != 1 /* Integer */) {
-			return 'TargetPosition: Integer required';
-		}
-		if ($variableCurrentPosition['VariableType'] != 1 /* Integer */) {
-			return 'CurrentPosition: Integer required';
-		}
-		if ($variablePositionState['VariableType'] != 1 /* Integer */) {
-			return 'PositionState: Integer required';
-		}
-		if ($variableTargetPosition['VariableCustomAction'] != '') {
-			$profileAction = $variableTargetPosition['VariableCustomAction'];
-		} else {
-			$profileAction = $variableTargetPosition['VariableAction'];
-		}
-		if (!($profileAction > 10000)) {
-			return 'TargetDoorState: Action required';
-		}
-		return 'OK';
-	}
+    public static function getPosition()
+    {
+        return 90;
+    }
+    public static function getCaption()
+    {
+        return 'Window';
+    }
+    public static function getColumns()
+    {
+        return [
+            [
+                'label' => 'TargetPosition',
+                'name'  => 'TargetPosition',
+                'width' => '150px',
+                'add'   => 0,
+                'edit'  => [
+                    'type' => 'SelectVariable'
+                ]
+            ],
+            [
+                'label' => 'CurrentPosition',
+                'name'  => 'CurrentPosition',
+                'width' => '150px',
+                'add'   => 0,
+                'edit'  => [
+                    'type' => 'SelectVariable'
+                ]
+            ],
+            [
+                'label' => 'PositionState',
+                'name'  => 'PositionState',
+                'width' => '150px',
+                'add'   => 0,
+                'edit'  => [
+                    'type' => 'SelectVariable'
+                ]
+            ]
+        ];
+    }
+    public static function getStatus($data)
+    {
+        if (!IPS_VariableExists($data['TargetPosition'])) {
+            return 'TargetPosition missing';
+        }
+        if (!IPS_VariableExists($data['CurrentPosition'])) {
+            return 'CurrentPosition missing';
+        }
+        if (!IPS_VariableExists($data['PositionState'])) {
+            return 'PositionState missing';
+        }
+        $variableTargetPosition = IPS_GetVariable($data['TargetPosition']);
+        $variableCurrentPosition = IPS_GetVariable($data['CurrentPosition']);
+        $variablePositionState = IPS_GetVariable($data['PositionState']);
+        if ($variableTargetPosition['VariableType'] != 1 /* Integer */) {
+            return 'TargetPosition: Integer required';
+        }
+        if ($variableCurrentPosition['VariableType'] != 1 /* Integer */) {
+            return 'CurrentPosition: Integer required';
+        }
+        if ($variablePositionState['VariableType'] != 1 /* Integer */) {
+            return 'PositionState: Integer required';
+        }
+        if ($variableTargetPosition['VariableCustomAction'] != '') {
+            $profileAction = $variableTargetPosition['VariableCustomAction'];
+        } else {
+            $profileAction = $variableTargetPosition['VariableAction'];
+        }
+        if (!($profileAction > 10000)) {
+            return 'TargetDoorState: Action required';
+        }
+        return 'OK';
+    }
 
 	public static function getTranslations()
 	{
