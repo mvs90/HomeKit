2eaae28e4d81001010caa27df25fa37694b2e355
Marc Schremmer <marc.schremmer@icloud.com>
2019-11-11 21:46:33 +0100

    update

--- HomeKitBridge/accessories/securitySystemTest.php
+++ HomeKitBridge/accessories/securitySystemTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-class HAPAccessorySecuritySystem extends HAPAccessoryBase
+class HAPAccessorySecuritySystemTest extends HAPAccessoryBase
 {
     use HelperSetDevice;
 	
