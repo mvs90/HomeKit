41099e3d00e1e05e952c12209b7445780a593658
Marc Schremmer <marc.schremmer@icloud.com>
2019-11-11 21:44:34 +0100

    test

---
+++ HomeKitBridge/accessories/securitySystemTest.php
@@ -0,0 +1,148 @@
+<?php
+
+declare(strict_types=1);
+
+class HAPAccessorySecuritySystem extends HAPAccessoryBase
+{
+    use HelperSetDevice;
+	
+	public function __construct($data)
+    {
+        parent::__construct(
+            $data,
+            [
+                new HAPServiceAccessoryInformation(),
+                new HAPServiceSecuritySystem()
+            ]
+        );
+    }
+	
+    public function notifyCharacteristicSecuritySystemCurrentState()
+    {
+        return [
+            $this->data['status']
+        ];
+    }
+	
+    public function readCharacteristicSecuritySystemCurrentState()
+    {
+        return GetValue($this->data['status']);
+		
+    }
+	
+    public function notifyCharacteristicStatusTampered()
+    {
+        return [
+            $this->data['sabotage']
+        ];
+    }
+	
+    public function readCharacteristicStatusTampered()
+    {
+        return GetValue($this->data['sabotage']);
+		
+    }
+    
+    public function notifyCharacteristicSecuritySystemTargetState()
+    {
+        return [
+            $this->data['status']
+        ];
+    }
+	
+    public function readCharacteristicSecuritySystemTargetState()
+    {
+        return GetValue($this->data['status']);
+    }
+	
+    public function writeCharacteristicSecuritySystemTargetState($value)
+    {
+        self::setDevice($this->data['status'], $value);
+    }
+}
+
+class HAPAccessoryConfigurationSecuritySystem
+{
+	use HelperSetDevice;
+	
+    public static function getPosition()
+    {
+        return 92;
+    }
+	
+    public static function getCaption()
+    {
+        return 'Security System Test';
+    }
+	
+    public static function getColumns()
+    {
+        return [
+            [
+                'label' => 'status',
+                'name'  => 'status',
+                'width' => '150px',
+                'add'   => 0,
+                'edit'  => [
+                    'type' => 'SelectVariable'
+                ]
+            ],
+            [
+                'label' => 'sabotage',
+                'name'  => 'sabotage',
+                'width' => '150px',
+                'add'   => 0,
+                'edit'  => [
+                    'type' => 'SelectVariable'
+                ]
+            ]
+        ];
+    }
+	
+    public static function getStatus($data)
+    {
+        if (!IPS_VariableExists($data['status'])) {
+            return 'Variable missing';
+        }
+        $targetVariable = IPS_GetVariable($data['status']);
+        if ($targetVariable['VariableType'] != 1 /* Integer */) {
+            return 'Int required';
+        }
+	    if ($targetVariable['VariableCustomProfile'] != '') {
+            $profileName = $targetVariable['VariableCustomProfile'];
+        } else {
+            $profileName = $targetVariable['VariableProfile'];
+        }
+        if (!IPS_VariableProfileExists($profileName)) {
+            return 'Profile required';
+        }
+        if ($targetVariable['VariableCustomAction'] != '') {
+            $profileAction = $targetVariable['VariableCustomAction'];
+        } else {
+            $profileAction = $targetVariable['VariableAction'];
+        }
+        if (!($profileAction > 10000)) {
+            return 'Action required';
+        }
+        if (!IPS_VariableExists($data['sabotage'])) {
+            return 'Variable missing';
+        }
+        return 'OK';
+    }
+	
+    public static function getTranslations()
+    {
+        return [
+            'de' => [
+                'Security System'    => 'Alarmanlage',
+                'status'            => 'status',
+                'Variable missing'      => 'Variable fehlt',
+                'Int required'          => 'Int benötigt',
+                'Profile required'      => 'Profil benötigt',
+                'OK'                    => 'OK'
+            ]
+        ];
+    }
+}
+
+HomeKitManager::registerAccessory('SecuritySystemTest');
