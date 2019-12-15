e5268b72d600ee98b516fe38ab5428559a2542e1
Marc Schremmer <marc.schremmer@icloud.com>
2019-12-15 19:38:59 +0100

    update

--- HomeKitBridge/tlv.php
+++ HomeKitBridge/tlv.php
@@ -215,7 +215,7 @@ class TLV8_Permissions extends TLV8
             case TLVPermissions::Admin:
                 return 'Admin';
             default:
-                throw new Exception('Invalid permission');
+                return 'Undefined';
         }
     }
 }
@@ -265,7 +265,7 @@ class TLVParser
             case TLVType::Separator:
                 return new TLV8_Separator($data);
             default:
-                return new TLV8($data);
+                throw new Exception('Unsupported TLV');
         }
     }
 
@@ -334,7 +334,7 @@ class TLVBuilder
         return self::Base(TLVType::Error, chr($error));
     }
 
-    public static function RetryDelay(string $retryDelay): string
+    public static function RetryDelay(int $retryDelay): string
     {
         return self::Base(TLVType::RetryDelay, pack('N', $retryDelay));
     }
